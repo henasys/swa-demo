cbc0bc5c89e74e8cb372998b89aa28b7d22f3aa7
Alex Hojong Lee <alexhojonglee@gmail.com>
2022-03-05 03:01:41 +0900

    back to the original

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,12 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-    }),
+    adapter: adapter(),
 
     // Override http methods in the Todo forms
     methodOverride: {
